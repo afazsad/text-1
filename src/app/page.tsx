d87a27155bb1cdeec0e61daa02814a4464c5d573
afazsad <afazsad709@gmail.com>
2025-05-01 14:55:20 +0330

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  useEffect(() => {
-    return 0
-  }, [])
   return (
-   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil tenetur minima praesentium, iste perspiciatis voluptas nam voluptate ratione labore non similique alias eum. Aperiam sapiente quidem deleniti, maxime corrupti illum!</p>
+   <p>96532.2121</p>
   );
 }
